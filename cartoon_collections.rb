d2e1d5798efec36787d6f98d58e864a169a88d69
HARPREET GHOTRA <harpreetghotra40@icloud.com>
2019-04-16 22:53:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(names)# code an argument here
-    names.each_with_index{ |name , index| puts "#{index} #{name}"}# Your code here
+    names.each_with_index{ |name , index| puts "#{index + 1} #{name}"}# Your code here
 end
 
 def summon_captain_planet# code an argument here
